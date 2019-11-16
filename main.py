cca03cd26a0eee966748fda4f4a6c02439226e25
erdal <erdalsidal99@gmail.com>
2019-11-16 15:35:45 +0300

    divide and conquer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def brute_force_max_subarray(array):
     length = len(array)
-    max_sum_index= (0,0)
+    max_sum_index = (0, 0)
     max_sum = 0
     for start in range(length):
         sum = 0
@@ -29,6 +29,37 @@ def linear_time_max_subarray(array):
     return array[start:end]
 
 
-array = [-2, -5, 6, -20, -3, 1, 5, -6]
-max_range = linear_time_max_subarray(array)
+def max_crossing_subarray(array, l, m, h):
+    left_max_sum = -1000000
+    sum_l = 0
+    for i in range(m, l - 1, -1):
+        sum_l = sum_l + array[i]
+        if sum_l > left_max_sum:
+            left_max_sum = sum_l
+
+    sum_r = 0
+    right_max_sum = -100000
+    for j in range(m + 1, h):
+        sum_r = sum_r + array[j]
+        if sum_r > right_max_sum:
+            right_max_sum = sum_r
+
+    return left_max_sum + right_max_sum
+
+def max_subarray(array, l, h):
+    m = int ((h + l) / 2)
+
+    if l == h:
+        return array[l]
+
+    return max(
+        max_subarray(array, l, m),
+        max_subarray(array, m + 1, h),
+        max_crossing_subarray(array, l, m, h)
+    )
+
+
+
+array = [-2, -5, 6, -2, -3, 1, 5, -6]
+max_range = max_subarray(array, 0, len(array)-1)
 print(max_range)

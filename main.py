ecd1d15e5119bf20c6b604f432a1ae9500bd7379
erdal <erdalsidal99@gmail.com>
2019-11-17 21:24:55 +0300

    start, end index fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import time
 
 def brute_force_max_subarray(array):
     length = len(array)
@@ -11,7 +10,7 @@ def brute_force_max_subarray(array):
             sum += array[end]
             if sum > max_sum:
                 max_sum = sum
-                max_sum_index = (start, end)
+                max_sum_index = (max_sum, start, end)
     return max_sum_index
 
 
@@ -20,16 +19,16 @@ def linear_time_max_subarray(array):
     max_ending_here = -10000
     start, end = 0, 0
     for index in range(len(array)):
-        max_ending_here = max_ending_here + array[index]
-        if max_ending_here < array[index] and array[index] >= max_so_far:
-            max_ending_here = array[index]
+        max_ending_here = max(array[index], max_ending_here + array[index])
+        if max_ending_here == array[index]:
             start = index
 
         if max_so_far < max_ending_here:
             max_so_far = max_ending_here
             end = index
+            final_start = start
 
-    return max_so_far, start, end
+    return max_so_far, final_start, end
 
 
 def max_crossing_subarray(array, l, m, h):
@@ -39,6 +38,7 @@ def max_crossing_subarray(array, l, m, h):
         sum_l = sum_l + array[i]
         if sum_l > left_max_sum:
             left_max_sum = sum_l
+            left_start_index = i
 
     sum_r = 0
     right_max_sum = -100000
@@ -46,46 +46,37 @@ def max_crossing_subarray(array, l, m, h):
         sum_r = sum_r + array[j]
         if sum_r > right_max_sum:
             right_max_sum = sum_r
+            right_end_index = j
 
-    return left_max_sum + right_max_sum
 
-def divide_and_conquer_max_subarray(array, l, h):
+    return left_max_sum + right_max_sum, left_start_index, right_end_index
+
+def max_subarray(array, l, h):
     m = int ((h + l) / 2)
 
     if l == h:
-        return array[l]
+        return array[l], l
 
     return max(
-        divide_and_conquer_max_subarray(array, l, m),
-        divide_and_conquer_max_subarray(array, m + 1, h),
+        max_subarray(array, l, m),
+        max_subarray(array, m + 1, h),
         max_crossing_subarray(array, l, m, h)
     )
 
+def generate_random_array(number):
+    output = []
+    for i in range(number):
+        output.append(random.randint(-100,100))
+    return output
 
-array_num_lower_bound = -10000
-array_num_upper_bound = 10000
-linear_time_arr=[]
-brute_time_arr=[]
-divide_and_conquer_max_subarray_time_arr=[]
 
-n_variations = [10000000, 100000000, 1000000000, 10000000000]
-for n in n_variations:
-    array = []
-    for _ in range(n):
-        array.append(random.randint(array_num_lower_bound,array_num_upper_bound))
+#array = [-2, -5, 6, -2, -3, 1, 5, -6]
+array = [-2, 7, 6, -4, -2, -3, -1, -5, -6]
+random_array = generate_random_array(100)
+max_range = max_subarray(random_array, 0, len(random_array) - 1)
+max_range1 = linear_time_max_subarray(random_array)
+max_range2 = brute_force_max_subarray(random_array)
 
-    start=time.time()
-    print("divide_and_conquer_max_subarray", n)
-    max_output = divide_and_conquer_max_subarray(array, 0, len(array) - 1)
-    end=time.time()
-    divide_and_conquer_max_subarray_time_arr.append(end - start)
-
-    start=time.time()
-    print("linear_time_max_subarray", n)
-    linear_output = linear_time_max_subarray(array)
-    end = time.time()
-    linear_time_arr.append(end - start)
-
-print("divide_and_conquer_max_subarray_time_arr", divide_and_conquer_max_subarray_time_arr)
-print("linear_time_arr", linear_time_arr)
-print("brute_time_arr", brute_time_arr)
+print(max_range)
+print(max_range1)
+print(max_range2)
